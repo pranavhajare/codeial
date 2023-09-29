889fee7fa07c5399456e4448af317b4d6be9a7b9
pranavhajare <hajare.pranav@gmail.com>
2023-09-29 19:24:16 +0530

    add view engine

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
 
 // use express router
-app.use('/',require('./routes'));
+app.use("/", require("./routes"));
 
-app.listen(port,function(err){
-    if(err){
-        console.log('Error',err);
-        console.log(`Error in running the server: ${err}`);
-    }
-    console.log(`server is running on port : ${port}`);
 
+// set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
+  console.log(`server is running on port : ${port}`);
 });
