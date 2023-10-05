af0b3440472e560fab453314771bcb2b5248557f
pranavhajare <hajare.pranav@gmail.com>
2023-10-05 14:18:55 +0530

    pasport installed

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,9 @@
-module.exports.home = function(req,res){
-    return res.end('<h1>Express is up for codeial');
-
+module.exports.home = function(req, res){
+    console.log(req.cookies);
+    res.cookie('user_id', 25);
+    return res.render('home', {
+        title: "Home"
+    });
 }
 
-// module.exprots.actionName = function(req,res){}
+// module.exports.actionName = function(req, res){}
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,16 @@
 const express = require('express');
 
 const router = express.Router();
-const homeController = require('../controllers/home_controller') 
-
+const homeController = require('../controllers/home_controller');
 
 console.log('router loaded');
 
-router.get('/',homeController.home);
-router.use('/users',require('./users'));
 
-// for any further routes, acces from here
-// router.use('routerName',require(./routerfile));
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
 
-
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
 
 
 module.exports = router;
