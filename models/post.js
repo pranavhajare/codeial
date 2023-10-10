ddd027962e1002a439f60970e42f6b3d2060555b
pranavhajare <hajare.pranav@gmail.com>
2023-10-10 14:19:43 +0530

    adding comment to databse error fixed

--- models/post.js
+++ models/post.js
@@ -11,6 +11,13 @@ const postSchema = new mongoose.Schema({
         ref : 'User'
 
     },
+    // include the array of ids of all comments in this post schema itself
+    comments: [
+        {
+            type:  mongoose.Schema.Types.ObjectId,
+            ref: 'comment'
+        }
+    ]
 
 },{
     timestamps :true
