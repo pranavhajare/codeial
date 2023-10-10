bdd2f0c5d064038275d66c15d240cca20ec482b1
pranavhajare <hajare.pranav@gmail.com>
2023-10-10 14:09:01 +0530

    adding comment to database

--- models/comment.js
+++ models/comment.js
@@ -28,5 +28,5 @@ const commentSchema = new mongoose.Schema({
 
 });
 
-const comment = mongoose.model('Comment',commentSchema);
+const Comment = mongoose.model('Comment',commentSchema);
 module.exports = Comment;
