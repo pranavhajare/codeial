7be8246e81abbc44c45e82e7f169f312fbe686f8
pranavhajare <hajare.pranav@gmail.com>
2023-10-10 23:34:34 +0530

    deleting a post

--- routes/posts.js
+++ routes/posts.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const passport  = require('passport');
 
-const postsController = require('../controllers/posts_controller');
+const postsController = require('../controllers/posts_Controller');
 
 router.post('/create',passport.checkAuthentication, postsController.create);
 
+router.get('/destroy/:id',passport.checkAuthentication,postsController.destroy);
 
 module.exports = router;
