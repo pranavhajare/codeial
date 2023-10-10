1d8f6a80d1f54b87061b798133eb483342cd5c56
pranavhajare <hajare.pranav@gmail.com>
2023-10-10 14:29:04 +0530

    Display comment and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -17,7 +17,12 @@ module.exports.home = function(req, res){
 // populate the user of each post
    Post.find({})
    .populate('user')
-//    .populate(path)
+   .populate({
+    path:'comments',
+    populate:{
+        path:'user',
+    }
+   })
    .exec(function(err,posts){
     return res.render('home', {
         title: "Codeial Home",
